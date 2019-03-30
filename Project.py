b2575f1660cd3f7db4ab6f377ebad672a1a67374
Liam-Higgins2018 <liammhiggins@hotmail.com>
2019-03-30 17:07:48 +0000

    User Interface validation and functional

--- Project.py
+++ Project.py
@@ -211,19 +211,34 @@ def match(infix, string):
     return (nfa.accept in current)
 # A few tests
 
-options = int(input("\nPlease enter:\n 1) To read a sample test of regular expressions and strings\n 2) To input custom user regular expressions and strigs\n\n"))
+options = int(input("\nPlease enter:\n 1) To read a sample test of regular expressions and strings\n " +
+                        "2) To input custom user regular expressions and strigs\n 3) To exit the code\n\n"))
 
-if options == 1:
-    #Sample Regular Expressions and Strings
-    infixes = ["a.b.c*", "a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
-    strings = ["", "abc", "abbc", "abcc", "abad", "abbbc"]
+#Loops through statements prompting the user until they Quit out of the menu 
+while True:
+    if options == 1:
+        #Sample Regular Expressions and Strings
+        infixes = ["a.b.c*", "a.(b|d).c*", "(a.(b|d))*", "a.(b.b)*.c"]
+        strings = ["", "abc", "abbc", "abcc", "abad", "abbbc"]
 
 
-    for i in infixes:
-        for s in strings:
-            print(match(i, s), i, s)
+        for i in infixes:
+            for s in strings:
+                print(match(i, s), i, s)
 
-elif options == 2:
-    userInfix = input("Please enter your regular expression in the infix notation: ")
-    userString = input("Please enter a string to compare against your infix notation: ")
-    print(match(userInfix, userString), userInfix, userString)
+    elif options == 2:
+        #Custom user infix and string
+        userInfix = input("Please enter your regular expression in the infix notation: ")
+        userString = input("Please enter a string to compare against your infix notation: ")
+        print(match(userInfix, userString), userInfix, userString)
+    
+    elif options == 3:
+        print("Goodbye!!\n")
+        break
+
+    else:
+        #User Validation 
+        print("Error Invalid Input\n")
+        
+    options = int(input("\nPlease enter:\n 1) To read a sample test of regular expressions and strings\n 2) To input custom user regular expressions and strigs\n 3) To exit the code\n\n"))
+
